53987327cb501a0a0ff444cfd81b1d8cb556553c
sayan06 <sayan06.wis@gmail.com>
2023-07-17 14:28:52 +0530

    Added Email

--- application/config/email.php
+++ application/config/email.php
@@ -17,11 +17,11 @@
 // // $config['smtp_user'] = 'info@persausive.in';
 // // $config['smtp_pass'] = 'info@8899';
 
-// $config['smtp_host'] = 'mail.persausive.in';
-// $config['smtp_port'] = '587';
-// $config['smtp_timeout'] = '80';
-// $config['smtp_user'] = 'hello@port10.sa';
-// $config['smtp_pass'] = '52Y%wi]CS#HK';
+$config['smtp_host'] = 'mail.persausive.in';
+$config['smtp_port'] = '587';
+$config['smtp_timeout'] = '80';
+$config['smtp_user'] = 'hello@port10.sa';
+$config['smtp_pass'] = '52Y%wi]CS#HK';
 
 // // $config['smtp_crypto'] = 'tls'; 
 // // $config['smtp_host'] = 'smtp.gmail.com';
@@ -30,21 +30,21 @@
 // // $config['smtp_user'] = 'anwaro@port10.sa';
 // // $config['smtp_pass'] = 'jnkcsxuxrrbeygpv';
 
-// $config['charset'] = 'utf-8';
-// $config['mailtype'] = 'html';
-// $config['wordwrap'] = TRUE;
-// $config['newline'] = "\r\n";
-
-
-$config['protocol'] = 'smtp';
-$config['smtp_host'] = 'smtp.zeptomail.in';
-$config['smtp_crypto'] = 'tls'; 
-$config['smtp_port'] = '587';
-$config['smtp_timeout'] = '800';
-$config['smtp_user'] = 'emailapikey';
-$config['smtp_pass'] = 'PHtE6r0PQujr3WUt8hECtvDuRceiY9t9/79vKVYTsdlBD6ACTE0Hqd95lzSyqxZ4V6EXF/bOyYhtt7OYs+/XcDq4ZD4YDmqyqK3sx/VYSPOZsbq6x00YslkTcUXeUobsdd5i0iLQs9baNA==';
 $config['charset'] = 'utf-8';
 $config['mailtype'] = 'html';
 $config['wordwrap'] = TRUE;
 $config['newline'] = "\r\n";
+
+
+// $config['protocol'] = 'smtp';
+// $config['smtp_host'] = 'smtp.zeptomail.in';
+// $config['smtp_crypto'] = 'tls'; 
+// $config['smtp_port'] = '587';
+// $config['smtp_timeout'] = '800';
+// $config['smtp_user'] = 'emailapikey';
+// $config['smtp_pass'] = 'PHtE6r0PQujr3WUt8hECtvDuRceiY9t9/79vKVYTsdlBD6ACTE0Hqd95lzSyqxZ4V6EXF/bOyYhtt7OYs+/XcDq4ZD4YDmqyqK3sx/VYSPOZsbq6x00YslkTcUXeUobsdd5i0iLQs9baNA==';
+// $config['charset'] = 'utf-8';
+// $config['mailtype'] = 'html';
+// $config['wordwrap'] = TRUE;
+// $config['newline'] = "\r\n";
 
